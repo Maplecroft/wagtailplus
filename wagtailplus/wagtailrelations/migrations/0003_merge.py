0996303b955588342c6977f80e374e3fa1557c60
James Rutherford <james.rutherford@maplecroft.com>
2015-11-23 15:34:25 +0000

    added merge migration & bumped version

---
+++ wagtailplus/wagtailrelations/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailrelations', '0002_auto_20151123_1203'),
+        ('wagtailrelations', '0002_auto_20151027_2150'),
+    ]
+
+    operations = [
+    ]
